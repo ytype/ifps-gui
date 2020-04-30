365a9059baf25f1922c62714e8c20dc90d370efc
ytype <59418226+ytype@users.noreply.github.com>
2020-05-01 02:10:51 +0900

    ENH: update design, dev upload & download

--- main-v0.1.py
+++ main-v0.1.py
@@ -11,6 +11,7 @@ class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
         self.file_name = ''
+        self.hash = ''
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(820, 550)
         MainWindow.setMinimumSize(QtCore.QSize(820, 550))
@@ -39,6 +40,7 @@ class Ui_MainWindow(object):
         self.pannel_btn.setPixmap(QtGui.QPixmap("design/png/init.png"))
         self.pannel_state = 'init'
         self.pannel_btn.setObjectName("pannel_btn")
+
         self.result_label = QtWidgets.QLabel(self.centralwidget)
         self.result_label.setGeometry(QtCore.QRect(320, 358, 441, 16))
         font = QtGui.QFont()
@@ -48,13 +50,28 @@ class Ui_MainWindow(object):
         self.result_label.setAutoFillBackground(False)
         self.result_label.setStyleSheet("color : white;")
         self.result_label.setObjectName("result_label")
-        self.file_label = QtWidgets.QLabel(self.centralwidget)
-        self.file_label.setGeometry(QtCore.QRect(320, 260, 431, 61))
+
+        self.download_btn = QtWidgets.QLabel(self.centralwidget)
+        self.download_btn.setGeometry(QtCore.QRect(728, 296, 32, 32))
+        self.download_btn.setText("")
+        self.download_btn.setObjectName("download_btn")
+
+        self.file_title_label = QtWidgets.QLabel(self.centralwidget)
+        self.file_title_label.setGeometry(QtCore.QRect(410, 220, 431, 61))
         font = QtGui.QFont()
-        font.setFamily("나눔바른고딕 UltraLight")
+        font.setFamily("나눔스퀘어 Light")
+        font.setPointSize(12)
+        self.file_title_label.setFont(font)
+        self.file_title_label.setObjectName("file_label")
+
+        self.file_label = QtWidgets.QLabel(self.centralwidget)
+        self.file_label.setGeometry(QtCore.QRect(410, 240, 431, 61))
+        font = QtGui.QFont()
+        font.setFamily("나눔스퀘어 Light")
         font.setPointSize(10)
         self.file_label.setFont(font)
         self.file_label.setObjectName("file_label")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -63,10 +80,18 @@ class Ui_MainWindow(object):
         self.file_icon.mouseReleaseEvent = self.file_select_upload
         self.browse_upload_btn.mouseReleaseEvent = self.file_select_upload
         self.pannel_btn.mouseReleaseEvent = self.pannel_click
+        self.download_btn.mouseReleaseEvent = self.download_file
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
+    def download_file(self, event):
+        if(self.hash != ''):
+            url = 'https://gateway.ipfs.io/ipfs/'
+            webbrowser.open(url+self.hash)
+        else:
+            self.result_label.setText('no file')
 
 
     def detectWin64(self):
@@ -106,7 +131,9 @@ class Ui_MainWindow(object):
         else:
             result = self.ipfs.add(self.file_name)
             if(result):
+                self.hash = result
                 self.file_label.setText(result)
+                self.file_title_label.setText(self.file_name.split('/')[-1])
             else:
                 self.result_label.setText('ipfs add error')
 
